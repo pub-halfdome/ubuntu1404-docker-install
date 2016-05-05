7016570a93b769f8126694e70a126f533be053c8
public@halfdome <public@halfdome.biz>
2016-05-05 20:03:41 +0900

    init

---
+++ docker-compose-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ubuntu Trusty 14.04 (LTS)
+
+su -c "curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose"
